13de262b6779d6fa9eb1b485d12ef69169050538
Zach Murrell <zamurrell@gmail.com>
2020-07-14 14:06:10 -0500

    Initial commit, config for TypeScript, basic server and API created with Express

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { listings } from "./listings";
+const app = express();
+const port = 9000;
+
+app.use(bodyParser.json());
+
+app.get("/listings", (_req, res) => {
+  return res.send(listings);
+});
+
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+  return res.send("Failed to delete listing");
+});
+
+app.listen(port);
+
+console.log(`[app]: http://localhost:${port}`);
