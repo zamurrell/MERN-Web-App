c7de08e070f82c0f0da6b313a6d06fd9419aeeb7
Zach Murrell <zamurrell@gmail.com>
2020-07-21 21:43:14 -0500

    Created UI, CRUD from client/UI

---
+++ client/src/sections/Listings/Listings.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { server } from "../../lib/api";
+import {
+  DeleteListingData,
+  DeleteListingVariables,
+  ListingsData,
+} from "./types";
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
+
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
+interface Props {
+  title: string;
+}
+
+export const Listings = ({ title }: Props) => {
+  const fetchListings = async () => {
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
+    console.log(data);
+  };
+
+  const deleteListing = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: "5f165051eb565123583bc7bd", // hardcoded id variable,
+      },
+    });
+    console.log(data); // check the console to see the result of the mutation!
+  };
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button onClick={fetchListings}>Query Listings</button>
+      <button onClick={deleteListing}>Delete a listing</button>
+    </div>
+  );
+};
