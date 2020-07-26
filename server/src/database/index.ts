7865faf1bfe89f4b3f76f92da88fedf824b42115
Zach Murrell <zamurrell@gmail.com>
2020-07-26 18:46:44 -0500

    Formatted collections and added clear db script

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-import { Database } from "../lib/types";
+import { Database, User, Listing, Booking } from "../lib/types";
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net`;
 
@@ -9,9 +9,12 @@ export const connectDatabase = async (): Promise<Database> => {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+
   const db = client.db("main");
 
   return {
-    listings: db.collection("test_listings"),
+    bookings: db.collection<Booking>("bookings"),
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users"),
   };
 };
