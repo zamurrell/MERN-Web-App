b8cd5f4637bf41fd207be7583559d070202ed71e
Zach Murrell <zamurrell@gmail.com>
2020-08-01 15:31:40 -0500

    Persistent login w/ cookie. X-CSRF token

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { render } from "react-dom";
-import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloProvider, useMutation } from "@apollo/react-hooks";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Layout, Affix } from "antd";
+import { Layout, Affix, Spin } from "antd";
+import { Operation } from "apollo-boost";
 
 import {
   AppHeader,
@@ -14,15 +15,30 @@ import {
   User,
   Login,
 } from "./sections";
+import { LOG_IN } from "./lib/graphql/mutations";
+import {
+  LogIn as LogInData,
+  LogInVariables,
+} from "./lib/graphql/mutations/LogIn/__generated__/LogIn";
 import { Viewer } from "./lib/types";
 import "./styles/index.css";
 import * as serviceWorker from "./serviceWorker";
+import { AppHeaderSkeleton, ErrorBanner } from "./lib/components";
 
 const ApolloBoost = require("apollo-boost");
+
 const ApolloClient = ApolloBoost.default;
 
 const client = new ApolloClient({
   uri: "/api",
+  request: async (operation: Operation) => {
+    const token = sessionStorage.getItem("token");
+    operation.setContext({
+      headers: {
+        "X-CSRF-TOKEN": token || "",
+      },
+    });
+  },
 });
 
 const initialViewer: Viewer = {
@@ -35,9 +51,45 @@ const initialViewer: Viewer = {
 
 const App = () => {
   const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  const [logIn, { error }] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+    onCompleted: (data) => {
+      if (data && data.logIn) {
+        setViewer(data.logIn);
+
+        if (data.logIn.token) {
+          sessionStorage.setItem("token", data.logIn.token);
+        } else {
+          sessionStorage.removeItem("token");
+        }
+      }
+    },
+  });
+
+  const logInRef = useRef(logIn);
+
+  useEffect(() => {
+    logInRef.current();
+  }, []);
+
+  if (!viewer.didRequest && !error) {
+    return (
+      <Layout className="app-skeleton">
+        <AppHeaderSkeleton />
+        <div className="app-skeleton__spin-section">
+          <Spin size="large" tip="Launching the app" />
+        </div>
+      </Layout>
+    );
+  }
+
+  const logInErrorBannerElement = error ? (
+    <ErrorBanner description="We weren't able to verify if you were logged in. Please try again later!" />
+  ) : null;
+
   return (
     <Router>
       <Layout id="app">
+        {logInErrorBannerElement}
         <Affix offsetTop={0} className="app__affix-header">
           <AppHeader viewer={viewer} setViewer={setViewer} />
         </Affix>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 import express, { Application } from "express";
+import cookieParser from "cookie-parser";
 import { ApolloServer } from "apollo-server-express";
 
 import { connectDatabase } from "./database";
@@ -8,19 +9,19 @@ import { typeDefs, resolvers } from "./graphql";
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
+
+  app.use(cookieParser(process.env.SECRET));
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db }),
+    context: ({ req, res }) => ({ db, req, res }),
   });
-  server.applyMiddleware({ app, path: "/api" });
 
+  server.applyMiddleware({ app, path: "/api" });
   app.listen(process.env.PORT);
 
-  console.log(`[app]: http://localhost:${process.env.PORT}`);
-
-  //   const listings = await db.listings.find({}).toArray();
-  //   console.log(listings);
+  console.log(`[app] : http://localhost:${process.env.PORT}`);
 };
 
 mount(express());
