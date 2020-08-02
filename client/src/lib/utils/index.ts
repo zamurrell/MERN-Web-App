4fd93739a1f946a97046f0bf475cf60be86ad417
Zach Murrell <zamurrell@gmail.com>
2020-08-01 21:20:15 -0500

    User listings and bookings, personal vs general info

--- client/src/lib/utils/index.ts
+++ client/src/lib/utils/index.ts
@@ -17,3 +17,10 @@ export const displaySuccessNotification = (
 export const displayErrorMessage = (error: string) => {
   return message.error(error);
 };
+
+export const formatListingPrice = (price: number, round = true) => {
+  const formattedListingPrice = round ? Math.round(price / 100) : price / 100;
+  return `$${formattedListingPrice}`;
+};
+
+export const iconColor = "#1890ff";
--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -2,5 +2,12 @@ import merge from "lodash.merge";
 
 import { userResolvers } from "./User";
 import { viewerResolvers } from "./Viewer";
+import { listingResolvers } from "./Listing";
+import { bookingResolvers } from "./Booking";
 
-export const resolvers = merge(userResolvers, viewerResolvers);
+export const resolvers = merge(
+  userResolvers,
+  viewerResolvers,
+  listingResolvers,
+  bookingResolvers
+);
